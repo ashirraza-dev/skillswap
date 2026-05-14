b6008c898b1dcd5e28202211a95be17c8b8031b2
ashirraza-dev <zmcmart@gmail.com>
2026-05-14 22:03:34 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,12 @@ import {
 // ============================================
 // Replace these placeholder values with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAjAgUdBwyu820JqxtAr3546J90trnNImI",
-  authDomain: "skillswap-48163.firebaseapp.com",
-  projectId: "skillswap-48163",
-  storageBucket: "skillswap-48163.firebasestorage.app",
-  messagingSenderId: "953886423467",
-  appId: "1:953886423467:web:47dcc723d89d9765de7aee"
+  apiKey: "AIzaSyCHBzeGpUogq0djc47g2FLa4y6e6EGv4uU",
+  authDomain: "skillswap-d4f65.firebaseapp.com",
+  projectId: "skillswap-d4f65",
+  storageBucket: "skillswap-d4f65.firebasestorage.app",
+  messagingSenderId: "539262375570",
+  appId: "1:539262375570:web:4196eecfadd80bfb39384b"
 }
 
 // Initialize Firebase
